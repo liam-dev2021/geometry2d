3858ca3ae7a99083f9222c868890abca790d7d21
liam-dev2021 <liampace2021@gmail.com>
2023-03-10 20:33:32 -0500

    linear 2D bezier curve setters

--- src/main/java/com/liampace/geom/curves/Line2f.java
+++ src/main/java/com/liampace/geom/curves/Line2f.java
@@ -23,14 +23,18 @@ public class Line2f implements Bezier2f {
         this.end = new Vector2f(x1, y1);
     }
 
-    public void set(Line2f line) {
-
+    public Line2f set(Line2f line) {
+        this.set(line.start.x, line.start.y, line.end.x, line.end.y);
+        return this;
     }
-    public void set(Vector2f start, Vector2f end) {
-
+    public Line2f set(Vector2f start, Vector2f end) {
+        this.set(start.x, start.y, end.x, end.y);
+        return this;
     }
-    public void set(float x0, float y0, float x1, float y1) {
-
+    public Line2f set(float x0, float y0, float x1, float y1) {
+        this.start.set(x0, y0);
+        this.end.set(x1, y1);
+        return this;
     }
 
     @Override
