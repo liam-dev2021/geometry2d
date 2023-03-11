7e3e551bad1b9b6d5fcd186d8d4b967fd6aa555c
liam-dev2021 <liampace2021@gmail.com>
2023-03-10 20:30:48 -0500

    linear 2D bezier curve implementation stubbed

---
+++ src/main/java/com/liampace/geom/curves/Line2f.java
@@ -0,0 +1,69 @@
+package com.liampace.geom.curves;
+
+import org.joml.Vector2f;
+
+import com.liampace.geom.Bezier2f;
+
+public class Line2f implements Bezier2f {
+
+    private Vector2f start, end;
+
+    public Line2f(Line2f other) {
+
+    }
+
+    public Line2f(Vector2f start, Vector2f end) {
+        
+    }
+
+    public Line2f(float x0, float y0, float x1, float y1) {
+
+    }
+
+    public void set(Line2f line) {
+        
+    }
+    public void set(Vector2f start, Vector2f end) {
+
+    }
+    public void set(float x0, float y0, float x1, float y1) {
+
+    }
+
+    @Override
+    public int getLength() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getLength'");
+    }
+
+    @Override
+    public Vector2f getPoint(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPoint'");
+    }
+
+    @Override
+    public Vector2f getPosition(float t, Vector2f dest) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+
+    @Override
+    public Vector2f getDerivative(float t, Vector2f dest) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDerivative'");
+    }
+
+    @Override
+    public int getInterceptsX(float[] dest) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInterceptsX'");
+    }
+
+    @Override
+    public int getInterceptsY(float[] dest) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInterceptsY'");
+    }
+    
+}
