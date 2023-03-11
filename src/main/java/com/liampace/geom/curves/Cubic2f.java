3e378c01b9c686f5e976af1fdb2a463755decab6
liam-dev2021 <liampace2021@gmail.com>
2023-03-11 08:56:56 -0500

    cubic bezier stubbed

---
+++ src/main/java/com/liampace/geom/curves/Cubic2f.java
@@ -0,0 +1,69 @@
+package com.liampace.geom.curves;
+
+import org.joml.Vector2f;
+
+import com.liampace.geom.Bezier2f;
+
+public class Cubic2f implements Bezier2f {
+    
+    public static final int LENGTH = 4;
+
+    private Vector2f start, controlA, controlB, end;
+
+    public Cubic2f(Cubic2f other) {
+
+    }
+    public Cubic2f(Vector2f start, Vector2f controlA, Vector2f controlB, Vector2f end) {
+
+    }
+    public Cubic2f(float x0, float y0, float x1, float y1, float x2, float y2, float x3, float y3) {
+
+    }
+
+    public Cubic2f set(Cubic2f other) {
+        return null;
+    }
+    public Cubic2f set(Vector2f start, Vector2f controlA, Vector2f controlB, Vector2f end) {
+        return null;
+    }
+    public Cubic2f set(float x0, float y0, float x1, float y1, float x2, float y2, float x3, float y3) {
+        return null;
+    }
+
+    @Override
+    public int getLength() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getLength'");
+    }
+
+    @Override
+    public Vector2f getPoint(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPoint'");
+    }
+    
+    @Override
+    public Vector2f getPosition(float t, Vector2f dest) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+    
+    @Override
+    public Vector2f getDerivative(float t, Vector2f dest) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDerivative'");
+    }
+    
+    @Override
+    public int getInterceptsX(int index, float[] dest) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInterceptsX'");
+    }
+    
+    @Override
+    public int getInterceptsY(int index, float[] dest) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInterceptsY'");
+    }
+
+}
