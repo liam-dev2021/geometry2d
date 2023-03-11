d685e087b1a3baf13d14f088b04375a80622aa94
liam-dev2021 <liampace2021@gmail.com>
2023-03-10 20:24:40 -0500

    base features of 2D bezier curve interface

---
+++ src/main/java/com/liampace/geom/Bezier2f.java
@@ -0,0 +1,92 @@
+package com.liampace.geom;
+
+import java.util.function.Consumer;
+
+import org.joml.Vector2f;
+
+public interface Bezier2f {
+    /**
+     * Returns the number of points that define the bezier curve.
+     * @return the number of points that define the bezier curve
+     */
+    public int getLength();
+
+    /**
+     * Returns the point at the specified index of the bezier curve.
+     * @param index the index of the desired point
+     * @return the point at the specified index of the bezier curve 
+     */
+    public Vector2f getPoint(int index);
+
+    /**
+     * Interpolates between all points of the bezier curve using the given interpolation factor {@code t}. Then stores the result in {@code dest}.
+     * If {@code t} is {@code 0.0} the result is the bezier's starting point.
+     * If {@code t} is {@code 1.0} the result is the bezier's ending point.
+     * @param t interpolation factor between [0-1] range
+     * @param dest will hold the result 
+     * @return {@code dest}
+     */
+    public Vector2f getPosition(float t, Vector2f dest);
+
+    /**
+     * Interpolates the derivative of the bezier curve using the given interpolation factor {@code t}. Then stores the result in {@code dest}.
+     * @param t interpolation factor between [0-1] range
+     * @param dest will hold the result 
+     * @return {@code dest}
+     */
+    public Vector2f getDerivative(float t, Vector2f dest);
+
+    /**
+     * Calculates the derivative vector and normalizes it using the given interpolation factor {@code t}. Then stores the result in {@code dest}.
+     * @param t interpolation factor between [0-1] range
+     * @param dest will hold the result 
+     * @return {@code dest}
+     */
+    default Vector2f getTangent(float t, Vector2f dest) {
+        return this.getDerivative(t, dest).normalize();
+    }
+
+    /**
+     * Calculates the tangent vector and rotates it +90 Degrees so that if the tangent was equal to {@code (1, 0)}, the result stored in {@code dest} would be {@code (0, 1)}.
+     * @param t interpolation factor between [0-1] range
+     * @param dest will hold the result 
+     * @return {@code dest}
+     */
+    default Vector2f getNormalCCW(float t, Vector2f dest) {
+        return this.getTangent(t, dest).perpendicular();
+    }
+
+    /**
+     * Calculates the tangent vector and rotates it -90 Degrees so that if the tangent was equal to {@code (1, 0)}, the result stored in {@code dest} would be {@code (0, -1)}.
+     * @param t interpolation factor between [0-1] range
+     * @param dest will hold the result 
+     * @return {@code dest}
+     */
+    default Vector2f getNormalCW(float t, Vector2f dest) {
+        return this.getNormalCCW(t, dest).negate();
+    }
+
+    /**
+     * Solves the bezier curve where the roots cross the X-Axis.
+     * @param dest will hold all real roots of this bezier curve
+     * @return number of roots found
+     */
+    public int getInterceptsX(float[] dest);
+
+    /**
+     * Solves the bezier curve where the roots cross the Y-Axis.
+     * @param dest will hold all real roots of this bezier curve
+     * @return number of roots found
+     */
+    public int getInterceptsY(float[] dest);
+
+    /**
+     * Performs the given action for each point that defines the bezier curve.
+     * @param action The action to be performed for each point
+     */
+    default void forEach(Consumer<Vector2f> action) {
+        for (int i = 0, length = this.getLength(); i < length; i++) {
+            action.accept(this.getPoint(i));
+        }
+    }
+}
