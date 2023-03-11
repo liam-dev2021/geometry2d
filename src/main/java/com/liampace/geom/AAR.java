5d2c97bacc422efef9d4744d204e60485b436c3b
liam-dev2021 <liampace2021@gmail.com>
2023-03-11 11:07:15 -0500

    Axis Aligned Rectangle stubbed

---
+++ src/main/java/com/liampace/geom/AAR.java
@@ -0,0 +1,29 @@
+package com.liampace.geom;
+
+import org.joml.Vector2f;
+import org.joml.Vector2fc;
+
+/**
+ * Axis Aligned Rectangle
+ */
+public class AAR {
+    
+    private Vector2f min, max;
+
+    public AAR() {}
+
+    public AAR setFromCenter(Vector2f pos, Vector2f radii) {
+        return null;
+    }
+    public AAR setFromBounds(Vector2f...positions) {
+        return null;
+    }
+    
+    public Vector2fc getMin() {
+        return null;
+    }
+    public Vector2fc getMax() {
+        return null;
+    }
+
+}
